fab3227895e758c76c4c95885128121117d8d201
Katz, Kenneth <kskatz@ncbi.nlm.nih.gov>
2022-08-24 15:49:28 -0400

    SRAB-314, update so now it will get the current version of db

--- init_db.sh
+++ init_db.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -eu
 
-curl -vf "https://ftp.ncbi.nlm.nih.gov/sra/dbs/human_filter.db" -o ./data/human_filter.db
+VERSION=$(curl "https://ftp.ncbi.nlm.nih.gov/sra/dbs/human_filter/current/version")
+cd data && curl -vf "https://ftp.ncbi.nlm.nih.gov/sra/dbs/human_filter/${VERSION}.human_filter.db" -o "${VERSION}.human_filter.db"
+ln -s "${VERSION}".human_filter.db human_filter.db
 exit 0
