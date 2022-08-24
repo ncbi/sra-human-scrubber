23b79039a914a7fdd3f6745026bc3ca71fce8b1b
Katz, Kenneth <kskatz@ncbi.nlm.nih.gov>
2022-08-24 16:41:22 -0400

    SRAB-314, add txt extension for clarity

--- init_db.sh
+++ init_db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eu
 
-VERSION=$(curl "https://ftp.ncbi.nlm.nih.gov/sra/dbs/human_filter/current/version")
+VERSION=$(curl "https://ftp.ncbi.nlm.nih.gov/sra/dbs/human_filter/current/version.txt")
 cd data && curl -vf "https://ftp.ncbi.nlm.nih.gov/sra/dbs/human_filter/${VERSION}.human_filter.db" -o "${VERSION}.human_filter.db"
 ln -s "${VERSION}".human_filter.db human_filter.db
 exit 0
