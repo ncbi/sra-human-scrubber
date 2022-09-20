2967c9f299a13031903d90ad33cf0ba25ca22c34
Katz, Kenneth <kskatz@ncbi.nlm.nih.gov>
2022-09-20 16:20:53 -0400

    SRAB-553, safer use of curl

--- init_db.sh
+++ init_db.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -eu
 
-VERSION=$(curl "https://ftp.ncbi.nlm.nih.gov/sra/dbs/human_filter/current/version.txt")
-cd data && curl -vf "https://ftp.ncbi.nlm.nih.gov/sra/dbs/human_filter/${VERSION}.human_filter.db" -o "${VERSION}.human_filter.db"
-ln -s "${VERSION}".human_filter.db human_filter.db
-exit 0
+VERSION=$(curl -f "https://ftp.ncbi.nlm.nih.gov/sra/dbs/human_filter/current/version.txt")
+if [ -n "$VERSION" ];
+  then
+  cd data && curl -vf "https://ftp.ncbi.nlm.nih.gov/sra/dbs/human_filter/${VERSION}.human_filter.db" -o "${VERSION}.human_filter.db"
+  ln -s "${VERSION}".human_filter.db human_filter.db
+  exit 0
+fi
