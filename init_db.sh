7f8b613370ae73d7b0e49161e72270aa9ff9de4c
Katz, Kenneth <kskatz@ncbi.nlm.nih.gov>
2022-09-23 10:24:45 -0400

    SRAB-553, add update

--- init_db.sh
+++ init_db.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
-set -eu
-
-VERSION=$(curl -f "https://ftp.ncbi.nlm.nih.gov/sra/dbs/human_filter/current/version.txt")
-if [ -n "$VERSION" ];
-  then
-  cd data && curl -vf "https://ftp.ncbi.nlm.nih.gov/sra/dbs/human_filter/${VERSION}.human_filter.db" -o "${VERSION}.human_filter.db"
-  ln -s "${VERSION}".human_filter.db human_filter.db
-  exit 0
-fi
+update_db.sh
