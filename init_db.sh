97464ec8a08e6749909d72e507efcb968a321124
Katz, Kenneth <kskatz@ncbi.nlm.nih.gov>
2023-05-05 15:15:16 -0400

    SRAB-553, just helpful warning about space

--- init_db.sh
+++ init_db.sh
@@ -2,7 +2,7 @@
 set -eu
 # logic: if the new db is here, do nothing
 # if the old db (named human_filter.db) exists and is not a link, remove and proceed
-# if the new db is not here, then check for symbolic link, unlink and proceed.
+# if the new db is not here, then check for symbolic link, unlink if needed and proceed.
 
 VERSION=$(curl -f "https://ftp.ncbi.nlm.nih.gov/sra/dbs/human_filter/current/version.txt")
 [[ -z "$VERSION" ]] && echo "Version  not retrieved." && exit 1
@@ -28,6 +28,7 @@ if  [ "$current_md5" != "$MD5" ];
         [[ -e "human_filter.db" ]] && rm "human_filter.db"
         ln -s  "human_filter.db.${VERSION}" "human_filter.db"
         echo "Successfully installed human_filter.db.${VERSION}."
+        echo "You may want to delete a previous human_filter db to econoimize disk space."
     else
       echo "my_md5 was $my_md5, but should be $MD5"
       exit 3
